e8490ce15eb703d5e9dca63788f43efcb694c8cd
BeaLjungbacke <beatrice.ljungbacke@gmail.com>
2016-09-21 15:59:19 +0200

    Added interface and classes

---
+++ ShirtShop/IOrder.cs
@@ -0,0 +1,13 @@
+﻿namespace ShirtShop
+{
+    public interface IOrder
+    {
+        string List { get; set; }
+        int OrderNumber { get; set; }
+        int PriceSum { get; set; }
+
+        void AddProduct();
+        void RemoveProduct();
+        string SendEmail();
+    }
+}
