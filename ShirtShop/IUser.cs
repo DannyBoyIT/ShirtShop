31de6e3fb8d596b884c7a95be7c20f5bdef3ffb9
Danny Oskar <d_iliovits@windowslive.com>
2016-09-21 15:57:39 +0200

    Added Database and User interfaces and classes

---
+++ ShirtShop/IUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShirtShop
+{
+    public interface IUser
+    {
+        string Address { get; set; }
+        string Email { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string MobileNumber { get; set; }
+        string Password { get; set; }
+        string Username { get; set; }
+
+        void LogIn();
+        void LogOut();
+    }
+}
