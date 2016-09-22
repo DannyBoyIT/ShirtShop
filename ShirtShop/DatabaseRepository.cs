576d69c03398f7788c182d1cc56565c0a06e901a
isabellaalstrom <isabella.alstrom@gmail.com>
2016-09-22 12:52:01 +0200

    ny lista med shirts

--- ShirtShop/DatabaseRepository.cs
+++ ShirtShop/DatabaseRepository.cs
@@ -23,19 +23,51 @@ namespace ShirtShop
 
             var hat = new Product();
             hats.Add(hat);
-            //hat.Quantity = 4;
             hat.Name = "Filthatt";
             hat.Price = 499;
             hat.ProductID = 111;
             hat.Size = "M";
-            hat.Type = "Hat";
+            hat.Type = "Hatt";
+
+            var hat2 = new Product();
+            hats.Add(hat2);
+            hat2.Name = "Solhatt";
+            hat2.Price = 249;
+            hat2.ProductID = 112;
+            hat2.Size = "S";
+            hat2.Type = "Hatt";
+
+            var hat3 = new Product();
+            hats.Add(hat3);
+            hat3.Name = "Keps";
+            hat3.Price = 399;
+            hat3.ProductID = 113;
+            hat3.Size = "L";
+            hat3.Type = "Hatt";
+
+            var hat4 = new Product();
+            hats.Add(hat4);
+            hat4.Name = "Fez";
+            hat4.Price = 999;
+            hat4.ProductID = 114;
+            hat4.Size = "M";
+            hat4.Type = "Hatt";
+
+            var hat5 = new Product();
+            hats.Add(hat5);
+            hat5.Name = "Keps";
+            hat5.Price = 399;
+            hat5.ProductID = 115;
+            hat5.Size = "S";
+            hat5.Type = "Hatt";
 
             return hats;
         }
 
         public void GetAllShirts()
         {
-            throw new NotImplementedException();
+            var shirts = new List<Product>();
+
         }
 
         public void GetAllShoes()
