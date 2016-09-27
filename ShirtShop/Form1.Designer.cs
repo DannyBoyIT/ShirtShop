c095880d133f49f7622d26a7f39d865ed82ca7c2
Danny Oskar <d_iliovits@windowslive.com>
2016-09-27 14:13:06 +0200

    FreshStart3

--- ShirtShop/Form1.Designer.cs
+++ ShirtShop/Form1.Designer.cs
@@ -146,7 +146,7 @@
             this.linkLabel1.TabIndex = 13;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Hitta";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            
             // 
             // comboBox2
             // 
