ba12df9cb6bf46caa6b0ceacc7a977e9e155eb80
sTaMiNa01 <Matilda.Lind@yh.nackademin.se>
2016-09-27 14:42:57 +0200

    Cart form added

---
+++ ShirtShop/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShirtShop
+{
+    public partial class Cart : Form
+    {
+        public Cart()
+        {
+            InitializeComponent();
+        }
+
+        private void lstItemsInCart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnAddItem.Visible = true;
+            btnRemoveItem.Visible = true;
+        }
+
+        private void btnClearCart_Click(object sender, EventArgs e)
+        {
+            lstItemsInCart.Items.Clear();
+        }
+    }
+}
